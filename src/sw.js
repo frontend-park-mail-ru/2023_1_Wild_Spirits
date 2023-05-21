568811e2b8d7605c38cff3a93b2ad7933ba57a58
mihaillatyshov <mihail.latyshov2@mail.ru>
2023-05-22 02:00:52 +0300

    small fixes

--- src/sw.js
+++ src/sw.js
@@ -2,16 +2,17 @@ const cacheName = "v1";
 
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches
-            .open(cacheName)
-            .then((cache) => {
-                return cache.addAll(["/", "/assets"]);
-            })
-            .catch(console.log("failed to open cache"))
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(["/", "/assets"]);
+        })
     );
 });
 
 this.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
     event.respondWith(
         caches.open(cacheName).then((cache) => {
             return fetch(event.request)
