b6b0b5cec1a4efd26dcda507c2978dc48d19663c
Nikita <rioskill@yandex.ru>
2023-05-22 02:23:43 +0300

    notification icon

--- src/components/Notification/NotificationButton.tsx
+++ src/components/Notification/NotificationButton.tsx
@@ -3,12 +3,26 @@ import {VDOM} from "modules/vdom";
 import { HoveredImg } from "components/Common/HoveredImg";
 import { store } from "flux";
 import { openNotificationModal } from "flux/slices/modalWindowSlice";
+import { LoadStatus } from "requests/LoadStatus";
 
 export function NotfificationButton() {
+    const getNotificationIconSrc = () => {
+        if (store.state.notification.invites.loadStatus === LoadStatus.DONE &&
+            store.state.notification.invites.data.length > 0) {
+            // console.log(store.state.notification.invites.data.length)
+            return "/assets/img/new-notification-icon.svg";
+        }
+        return "/assets/img/notification-icon.svg";
+    }
+
+
+
+    // const hasNotifications = store.state.notification.invites.loadStatus === LoadStatus.DONE && ;
+
     return (
         <HoveredImg
             alt=""
-            src="/assets/img/notification-icon.svg"
+            src={getNotificationIconSrc()}
             iconClassName="notification-icon"
             onClick={() => store.dispatch(openNotificationModal())}
         />
